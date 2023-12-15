d83c11eeab6e4cb8918aeecd7d3598c1246aa8c5
Luka Pitskhelauri <luka.pitsk@gmail.com>
2023-12-15 18:06:27 +0400

    add layout and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/layout/header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/layout';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
