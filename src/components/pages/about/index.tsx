de22b6660c9a4d081dd83e981c9e5e217168b66b
Luka Pitskhelauri <luka.pitsk@gmail.com>
2023-12-17 21:30:29 +0400

    Add about page

--- src/components/pages/about/index.tsx
+++ src/components/pages/about/index.tsx
@@ -1,9 +1,38 @@
+import { AboutDiv } from "./about.styled"
+
 const About = () => {
 
     return (
-        <div>
-            Coming Soon...
-        </div>
+        <AboutDiv>
+            Welcome to LukaStore, where innovation meets excellence! <br /> <br /> We are dedicated to bringing you a curated selection of top-notch products that seamlessly blend quality, functionality, and style. As a customer-centric company, we take pride in providing solutions that enhance your everyday life.
+            <br />
+            <br />
+            Our Mission: <br /><br />
+            At LukaStore, our mission is to simplify and elevate your experiences by offering a range of products designed to meet your diverse needs. Whether you're looking for cutting-edge technology, stylish accessories, or practical everyday essentials, we have carefully curated our collection to reflect the latest trends and advancements.
+            <br />
+            <br />
+            Quality Assurance:<br /><br />
+            We understand the importance of quality, and that's why every product in our catalog undergoes rigorous testing to ensure it meets the highest standards. We partner with reputable manufacturers and designers who share our commitment to excellence. Rest assured, when you choose LukaStore, you're choosing products that are built to last.
+            <br />
+            <br />
+            Innovation at the Core:<br /><br />
+            In a world that's constantly evolving, we stay ahead of the curve by embracing innovation. Our team is passionate about exploring the latest technologies and design trends to bring you products that not only meet but exceed your expectations. We believe in pushing boundaries and redefining what's possible.
+            <br />
+            <br />
+            Customer-Centric Approach:<br /><br />
+            Your satisfaction is our priority. We strive to create a seamless shopping experience from start to finish. Our customer support team is always ready to assist you, ensuring that any queries or concerns are addressed promptly. Your feedback is invaluable, and we actively listen to it to continuously improve our offerings and services.
+            <br />
+            <br />
+            Environmental Responsibility:<br /><br />
+            As a responsible business, we are committed to minimizing our environmental impact. We actively seek sustainable and eco-friendly alternatives in our product selection and packaging. By choosing LukaStore, you join us in promoting a greener and healthier planet.
+            <br />
+            <br />
+            Join the LukaStore Community:<br /><br />
+            We believe in the power of community, and we invite you to join our ever-growing family of satisfied customers. Follow us on social media to stay updated on the latest releases, promotions, and to connect with like-minded individuals who share a passion for quality and innovation.
+            <br />
+            <br />
+            Thank you for choosing LukaStore. We look forward to being a part of your journey and providing you with exceptional products that enrich your lifestyle. Explore our catalog and discover a world of possibilities!
+        </AboutDiv>
     )
 }
 
